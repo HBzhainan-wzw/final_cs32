db5c8159d9150f6bd527d6387591df3860a3b5d1
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2020-12-13 13:45:15 -0800

    Add files via upload

--- problem2/testMatrix1.cpp
+++ problem2/testMatrix1.cpp
@@ -17,6 +17,9 @@ std::string toString(const Matrix & matrix ){
 }
 
 int main() {
+
+  bool caughtException = false;
+
   Matrix matrix_1 = {{1,2,3,4}, {4,5,6,6},{1,2,3,4}};
   Matrix matrix_2 = {{1,2,3,1,1,3,3}, {4,5,4,2,2,3,3},{7,8,5,3,3,3,3},{1,1,1,4,4,3,3}};
   Matrix matrix_3;
@@ -24,6 +27,37 @@ int main() {
 
   Matrix expected = {{34, 40, 30, 30, 30, 30, 30},{72, 87, 68, 56, 56, 63, 63},{34, 40, 30, 30, 30, 30, 30}};
   ASSERT_EQUALS(toString(expected), toString(matrix_3));
+
+  Matrix matrix_4 = {{1,2,3,4}, {4,5,6,6}};
+  Matrix matrix_5 = {{1,2,3,1,1,3,3}, {4,5,4,2,2,3,3},{7,8,5,3,3,3,3},{1,1,1,4,4,3,3}};
+  Matrix matrix_6 = {};
+  Matrix matrix_7 = {{1,2,3,1,1,3}, {4,5,4,2,2,3,3},{7,8,5,3,3,3,3},{1,1,1,4,4,3,3}};
+
+
+	try {
+    matrix_3 = matrixMultiply1(matrix_4, matrix_5);		
+	} catch (invalid_argument e) {
+		caughtException = true;
+	}
+	ASSERT_EQUALS(true, caughtException);
+	caughtException = false;
+
+
+	try {
+    matrix_3 = matrixMultiply1(matrix_4, matrix_6);		
+	} catch (invalid_argument e) {
+		caughtException = true;
+	}
+	ASSERT_EQUALS(true, caughtException);
+	caughtException = false;
+
+	try {
+    matrix_3 = matrixMultiply1(matrix_2, matrix_7);		
+	} catch (invalid_argument e) {
+		caughtException = true;
+	}
+	ASSERT_EQUALS(true, caughtException);
+	caughtException = false;
 
   return 0;
 }
