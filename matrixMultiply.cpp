8b996b0671530340a503d7c60446863c61f1b594
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2020-12-13 02:44:52 -0800

    Add files via upload

--- matrixMultiply.cpp
+++ matrixMultiply.cpp
@@ -1,5 +1,5 @@
 #include "matrixMultiply.h"
-
+using namespace std;
 // Part 1
 Matrix matrixMultiply1(const Matrix & matrix_1, const Matrix & matrix_2){
     //empty
@@ -9,10 +9,10 @@ Matrix matrixMultiply1(const Matrix & matrix_1, const Matrix & matrix_2){
     if(matrix_2[0].empty()){throw invalid_argument("-1");}    
 
     // not matrix
-    for(int i = 0; i < matrix_1.size();i++){
+    for(size_t i = 0; i < matrix_1.size();i++){
         if(matrix_1[i].size()!=matrix_1[0].size()){throw invalid_argument("-1");}
     }
-    for(int i = 0; i < matrix_2.size();i++){
+    for(size_t i = 0; i < matrix_2.size();i++){
         if(matrix_2[i].size()!=matrix_2[0].size()){throw invalid_argument("-1");}
     }
 
@@ -21,13 +21,13 @@ Matrix matrixMultiply1(const Matrix & matrix_1, const Matrix & matrix_2){
 
     Matrix res = {};
     //have the result size
-    int nRow = matrix_1.size();
-    int nCol = matrix_2[0].size();
-    for(int i = 0; i < nRow;i++){
+    size_t nRow = matrix_1.size();
+    size_t nCol = matrix_2[0].size();
+    for(size_t i = 0; i < nRow;i++){
         Array row = {};
-        for(int j = 0; j < nCol; j++){
-            int tempS = 0;
-            for (int k = 0; k < matrix_1[0].size(); k++)
+        for(size_t j = 0; j < nCol; j++){
+            size_t tempS = 0;
+            for (size_t k = 0; k < matrix_1[0].size(); k++)
             {
                 /* code */
                 tempS= tempS + matrix_1[i][k] * matrix_2[k][j];
@@ -48,10 +48,10 @@ Matrix matrixMultiply2(const Matrix & matrix_1, const Matrix & matrix_2){
     if(matrix_2[0].empty()){throw invalid_argument("-1");}    
 
     // not matrix
-    for(int i = 0; i < matrix_1.size();i++){
+    for(size_t i = 0; i < matrix_1.size();i++){
         if(matrix_1[i].size()!=matrix_1[0].size()){throw invalid_argument("-1");}
     }
-    for(int i = 0; i < matrix_2.size();i++){
+    for(size_t i = 0; i < matrix_2.size();i++){
         if(matrix_2[i].size()!=matrix_2[0].size()){throw invalid_argument("-1");}
     }
 
@@ -60,23 +60,23 @@ Matrix matrixMultiply2(const Matrix & matrix_1, const Matrix & matrix_2){
 
     Matrix res = {};
     //have the result size
-    int nRow = matrix_1.size();
-    int nCol = matrix_2[0].size();
+    size_t nRow = matrix_1.size();
+    size_t nCol = matrix_2[0].size();
 
     Matrix trans = {};
-    for(int i = 0; i < nRow;i++){
+    for(size_t i = 0; i < nRow;i++){
         Array temp = {};
-        for (int j = 0; j < matrix_2.size(); j++)
+        for (size_t j = 0; j < matrix_2.size(); j++)
         {
             temp.push_back(matrix_2[j][i]);
         }
         trans.push_back(temp);
     }    
 
-    for(int i = 0; i < nRow;i++){
+    for(size_t i = 0; i < nRow;i++){
         Array row = {};
-        for(int j = 0; j < nCol; j++){
-            int mul = inner_product(matrix_1[i].begin(),matrix_1[i].end(), trans[i].begin(),0);
+        for(size_t j = 0; j < nCol; j++){
+            size_t mul = inner_product(matrix_1[i].begin(),matrix_1[i].end(), trans[i].begin(),0);
             row.push_back(mul);
         }
         res.push_back(row);
@@ -89,7 +89,7 @@ Matrix matrixMultiply2(const Matrix & matrix_1, const Matrix & matrix_2){
 
 int op(Array ar1, Array ar2){
     int result = 0;
-    for (int i = 0; i < ar1.size(); i++)
+    for (size_t i = 0; i < ar1.size(); i++)
     {
         /* code */
         result += ar1[i]+ar2[i];
@@ -107,10 +107,10 @@ Matrix matrixMultiply3(const Matrix & matrix_1, const Matrix & matrix_2){
     if(matrix_2[0].empty()){throw invalid_argument("-1");}    
 
     // not matrix
-    for(int i = 0; i < matrix_1.size();i++){
+    for(size_t i = 0; i < matrix_1.size();i++){
         if(matrix_1[i].size()!=matrix_1[0].size()){throw invalid_argument("-1");}
     }
-    for(int i = 0; i < matrix_2.size();i++){
+    for(size_t i = 0; i < matrix_2.size();i++){
         if(matrix_2[i].size()!=matrix_2[0].size()){throw invalid_argument("-1");}
     }
 
@@ -119,23 +119,23 @@ Matrix matrixMultiply3(const Matrix & matrix_1, const Matrix & matrix_2){
     
     Matrix res = {};
     
-    int nRow = matrix_1.size();
-    int nCol = matrix_2[0].size();
+    size_t nRow = matrix_1.size();
+    // size_t nCol = matrix_2[0].size();
 
     
     Matrix trans = {};
-    for(int i = 0; i < nRow;i++){
+    for(size_t i = 0; i < nRow;i++){
         Array temp = {};
-        for (int j = 0; j < matrix_2.size(); j++)
+        for (size_t j = 0; j < matrix_2.size(); j++)
         {
             temp.push_back(matrix_2[j][i]);
         }
         trans.push_back(temp);
     }    
-    for (int i = 0; i < nRow; i++) {
+    for (size_t i = 0; i < nRow; i++) {
         Matrix tempM = {};
-        for (int j = 0; j < nRow; j++){
-            tempM.push_back(matrix_1);
+        for (size_t j = 0; j < nRow; j++){
+            tempM.push_back(matrix_1[i]);
         }
         res.push_back(Array(nRow,0));
         transform(tempM.begin(), tempM.end(), trans.begin(), res[i].begin(), op);
