eff86bec70f83d16994c1075625df8f8e3fdd27a
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2020-12-13 02:47:13 -0800

    Add files via upload

--- problem2/matrixMultiply.h
+++ problem2/matrixMultiply.h
@@ -5,7 +5,6 @@
 #include <numeric>
 #include <vector>
 #include <algorithm>
-#include <cstring>
 #include <functional> // std::multiplies  
 
 typedef std::vector<int> Array;
@@ -15,9 +14,5 @@ Matrix matrixMultiply1(const Matrix & matrix_1, const Matrix & matrix_2);
 Matrix matrixMultiply2(const Matrix & matrix_1, const Matrix & matrix_2);
 Matrix matrixMultiply3(const Matrix & matrix_1, const Matrix & matrix_2);
 
-class invalid_argument{
-    public:
-        invalid_argument(std::string msg);
-};
 
 #endif
